6b44933b24f476be32c5b180202afb0eec417c0d
Paul Merlin <paul@gradle.com>
2023-06-02 17:01:30 +0200

    refine

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-tasks.register<WriteProperties>("updateSomething") {
-    property("same", "always")
-    property("changing", System.currentTimeMillis())
-    destinationFile.set(layout.projectDirectory.file("some.properties"))
+tasks.register<WriteProperties>("updateAgpVersions") {
+    property("latests", "7.4.3,8.0.2,8.1.0-beta03,8.2.0-alpha06")
+    property("nightlyBuildId", System.currentTimeMillis())
+    property("nightlyVersion", "8.2.0-dev")
+    destinationFile.set(layout.projectDirectory.file("agp.properties"))
 }
