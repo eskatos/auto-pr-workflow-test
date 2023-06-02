0a2472cb3cfe8182b752a1d247a28cad27620247
Paul Merlin <paul@gradle.com>
2023-06-02 18:21:30 +0200

    fix atop automation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-description = "Dummy project and a task to update some properties file"
+description = "Dummy project with a task to update some properties file"
 
 tasks.register<WriteProperties>("updateAgpVersions") {
     property("latests", "7.4.3,8.0.2,8.1.0-beta03,8.2.0-alpha06")
